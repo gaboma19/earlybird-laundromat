e8732b7b7fca38946d3568a5315183a937ae9929
Gabriel Ma <gaboma19@gmail.com>
2023-10-29 21:28:15 -0400

    select buttons in title screen for gamepad

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -2,11 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class MainMenuController : MonoBehaviour
 {
+    [SerializeField] private EventSystem eventSystem;
+    [SerializeField] private GameObject muteButton;
+    [SerializeField] private GameObject playButton;
+    [SerializeField] private GameObject unmuteButton;
     public void PlayGame()
     {
         SceneManager.LoadScene("Laundromat");
+    }
+
+    public void SelectMuteButton()
+    {
+        eventSystem.SetSelectedGameObject(muteButton);
+    }
+
+    public void SelectPlayButton()
+    {
+        eventSystem.SetSelectedGameObject(playButton);
+    }
+
+    public void SelectUnmuteButton()
+    {
+        eventSystem.SetSelectedGameObject(unmuteButton);
     }
 }
